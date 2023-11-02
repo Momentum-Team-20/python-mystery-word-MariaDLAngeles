6232d320202850e3a1bdf605837ff92f37a93c6e
Maria Angeles <maria.d.juri@gmail.com>
2023-11-02 18:37:07 -0400

    turned guesses into loop, can win or lose. Need to change how remaining tries and letter guessing shows up in terminal

--- mystery_word.py
+++ mystery_word.py
@@ -1,4 +1,5 @@
 import random
+MAX_INCORRECT_TRIES = 8
 
 
 def play_game():
@@ -11,20 +12,27 @@ def play_game():
     guesses = []
     display = ["_" for character in chosen_word]
     print(display)
-    tries = 8
-    guess = input("Guess a letter!")
-    print(guess)
-    guesses.append(guess)
-    if guess in chosen_word:
-        print('correct!')
-        for index in range(len(chosen_word)):
-            if guess == chosen_word[index]:
-                display[index] = guess
-                print(display)
-    else:
-        print('incorrect!')
-        
-    
+    remaining_tries_counter = 8
+    # start loop here
+    while remaining_tries_counter > 0:
+        guess = input("Guess a letter!")
+        print(guess)
+        guesses.append(guess)
+        if guess in chosen_word:
+            print('correct!')
+            for index in range(len(chosen_word)):
+                if guess == chosen_word[index]:
+                    display[index] = guess
+                    print(display)
+                    print('chosen_word: ',chosen_word)
+                    print(guesses)
+            if chosen_word == ''.join(display):
+                print('you won!')
+        else:
+            remaining_tries_counter = remaining_tries_counter-1
+            print('incorrect!')
+        print('You have ', f'{remaining_tries_counter}', ' tries remaining')
+    print('Game Over!')
     # letters = [letter for letter in chosen_word]
     # print(letters)
         # blanks = '_' * len(chosen_word)
@@ -34,8 +42,6 @@ def play_game():
         # print("Guess a letter!")
         # print("Number of tries: ", f'{tries}')
         # print('\n')
-
-
 
 
 if __name__ == "__main__":
