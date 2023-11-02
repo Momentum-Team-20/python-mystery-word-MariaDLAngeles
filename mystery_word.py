5fae6a1ac2c8e23e617bbaf844a1c1b6910b5deb
Maria Angeles <maria.d.juri@gmail.com>
2023-11-02 14:31:20 -0400

    can read file, list words, and choose word for game - started on turning into blanks

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,18 @@
+import random
+
+
 def play_game():
-    pass
+    # read the words
+    f = open("words.txt", 'r')
+    # print(f.read())
+    # randomly pick one word
+    all_words = f.read()
+    word_list = all_words.split()
+    chosen_word = random.choice(word_list)
+    print(chosen_word)
+    # displaying chosen word as blanks by taking the length of the word and * it by _'s
+    blanks = '_' * len(chosen_word)
+    print(blanks) 
 
 
 if __name__ == "__main__":
