1f9ab1d54313a9af662507acd8d8cc41d640a091
Maria Angeles <maria.d.juri@gmail.com>
2023-11-03 17:46:25 -0400

    cleaned up code, display text, and comments

--- mystery_word.py
+++ mystery_word.py
@@ -15,59 +15,44 @@ def play_game():
     remaining_tries_counter = 8
     # start loop here
     while remaining_tries_counter > 0 and chosen_word != ''.join(display):
-        # this is true for when the game is lost
-        # game is won when the loop is false
-        guess = input("Guess a letter! ").lower()
-        # we need to check that our guess is valid(no repeats, no more than 1 letter)
+        guess = input("Guess a letter: ").lower()
+        # first we check for valid user inputs (no numbers, no #$@*!)
         if len(guess) > 1:
-            print('Guess ONE letter at a time!')
+            print('Invalid entry: Only 1 letter at a time.')
         elif not guess.isalpha():
-            print('Guess a LETTER!')
+            print('Invalid entry: Letters only.')
         elif guess in guesses:
             print("You already guessed that!")
-            print('Guess List/Array', guesses)
-        # getting here means the input is valid and now we check the guess for right or wrong
+            # print('Guess List/Array', guesses)
+        # next we check if the user guess is right or wrong
         else:
-            # then we append the guess to our array/list and then do the rest
+            # right or wrong, the user guess need to be stored in guesses
             guesses.append(guess)
-            # this is if you guessed right
+            # if you guessed RIGHT
             if guess in chosen_word:
-                print('correct!')
+                print('Correct!')
                 for index in range(len(chosen_word)):
                     if guess == chosen_word[index]:
                         display[index] = guess
                         # print('chosen_word: ', chosen_word)
                         # print(guesses)
                 if chosen_word == ''.join(display):
-                    # remaining_tries_counter = 0
                     print('You won!')
-                    # this is where you win and the game ends
-            # this is if you guessed wrong
+            # if you guessed WRONG
             else:
-                # changing this to elif to see if we can add an else for losing the game
                 remaining_tries_counter = remaining_tries_counter-1
-                print('Nope! You have ', f'{remaining_tries_counter} tries left')
-        # added this else statement and changed above to elif
+                print('Nope! You have ', remaining_tries_counter, 'tries left')
+        # right or wrong, we print display
             print(display)
+        # if tries run out, we reveal the chosen word
             if remaining_tries_counter == 0:
                 print("Oh no! Your mystery word was:", chosen_word)
-    # print('You finished the game!')
+    # win or lose, we need to know if the player wants to play again
     play_again = input('Want to play again? Y/N ').lower()
     if play_again == 'y':
         play_game()
     else:
         exit()
-
-    # this is where you lose and the game ends
-    # letters = [letter for letter in chosen_word]
-    # print(letters)
-        # blanks = '_' * len(chosen_word)
-        # guessed_letters = []
-        # guessed = False
-        # tries = 8
-        # print("Guess a letter!")
-        # print("Number of tries: ", f'{tries}')
-        # print('\n')
 
 
 if __name__ == "__main__":
